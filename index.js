d5995766e43b0a38db8edcdd9debf3759e8c4829
Shisun <worlddominator007@gmail.com>
2021-04-30 19:07:06 +0700

    change from black&white to random color

--- index.js
+++ index.js
@@ -2,41 +2,67 @@ const date = document.querySelector("#date");
 date.textContent = new Date().getFullYear();
 const grid = document.querySelector(".grid_container");
 const reset = document.querySelector("#reset");
+const containerSize = getComputedStyle(
+  document.documentElement
+).getPropertyValue("--size");
 
-const appendItemToSketch = (opt) => {
-  //get sketch custom variable thru compute style
-  const containerSize = getComputedStyle(
-    document.documentElement
-  ).getPropertyValue("--size");
+function generateRandomColor() {
+  return Math.floor(Math.random() * 256);
+}
 
-  let i = Math.pow(parseInt(containerSize), 2);
-  while (i > 0) {
-    const gridItem = document.createElement("div");
-    gridItem.classList = "grid_item";
-    grid.append(gridItem);
-    i--;
-  }
-  addMouseOverandTouchStart();
-};
-
-document.body.onload = appendItemToSketch(addMouseOverandTouchStart);
+function setGridItemColor(randomColor, targetNode) {
+  const red = randomColor();
+  const green = randomColor();
+  const blue = randomColor();
+  targetNode.target.style.setProperty("--red", `${red}`);
+  targetNode.target.style.setProperty("--green", `${green}`);
+  targetNode.target.style.setProperty("--blue", `${blue}`);
+}
 
 function addMouseOverandTouchStart() {
   const gridItem = document.querySelector(".grid_container").childNodes;
 
   gridItem.forEach((e) => {
     e.addEventListener("mouseover", (e) => {
+      setGridItemColor(generateRandomColor, e);
       e.target.classList.toggle("grid_item_too");
       e.stopPropagation();
     });
   });
 }
 
+function setGridSize() {
+  let size = prompt("Set Box Size (1 ~ 30)");
+  while (isNaN(size) || Number(size) < 1 || Number(size) > 30) {
+    size = prompt("Set Box Size (1 ~ 30)");
+  }
+  return Number(size);
+}
+
+const appendItemToSketch = (mouseEvent, size) => {
+  let i = Math.pow(parseInt(size), 2);
+  while (i > 0) {
+    const gridItem = document.createElement("div");
+    gridItem.classList = "grid_item";
+    grid.append(gridItem);
+    i--;
+  }
+  mouseEvent();
+};
+
+document.body.onload = appendItemToSketch(
+  addMouseOverandTouchStart,
+  containerSize
+);
+
 reset.onclick = () => {
   const gridItem = document.querySelector(".grid_container").childNodes;
+  //remove old item
   Array.from(gridItem).forEach((e) => {
     grid.removeChild(e);
   });
-  grid.style.setProperty("--size", "16");
-  appendItemToSketch(addMouseOverandTouchStart);
+  //set new grid
+  const size = setGridSize();
+  grid.style.setProperty("--size", `${size}`);
+  appendItemToSketch(addMouseOverandTouchStart, `${size}`);
 };
